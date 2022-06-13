2bb2a83a1b1e3c2109fff3247480f745458b411e
omersaldiran <osaldiran@gmail.com>
2022-06-13 22:31:38 +0300

    Core katmanına Result sınıfları eklendi

--- src/main/java/com/omersaldiran/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/omersaldiran/springbootdemo/SpringbootdemoApplication.java
@@ -3,14 +3,30 @@ package com.omersaldiran.springbootdemo;
 import com.omersaldiran.springbootdemo.business.concretes.ProductManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringbootdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootdemoApplication.class, args);
         System.out.println("Hello Spring!");
-
     }
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 
 }
---
+++ src/main/java/com/omersaldiran/springbootdemo/core/utilities/results/ErrorResult.java
@@ -0,0 +1,11 @@
+package com.omersaldiran.springbootdemo.core.utilities.results;
+
+public class ErrorResult extends Result{
+    public ErrorResult(String message) {
+        super(false, message);
+    }
+
+    public ErrorResult(){
+        super(false);
+    }
+}
